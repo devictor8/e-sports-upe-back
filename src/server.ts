8b86dc18d29285bdc4402e064bc74c9cca083503
devictor8 <joaosalgado605@gmail.com>
2025-06-20 14:50:43 -0300

    chore: ajustando host para 0.0.0.0 e configurando CORS

--- src/server.ts
+++ src/server.ts
@@ -9,7 +9,7 @@ import {
   userController,
   teamController,
 } from "./controllers";
-
+import cors from "@fastify/cors";
 import authPlugin from "./plugin/jwt-plugin";
 import { authController } from "./controllers/auth-controller";
 import { env } from "./config/env";
@@ -17,6 +17,9 @@ import fastifySwagger from "@fastify/swagger";
 import fastifySwaggerUi from "@fastify/swagger-ui";
 
 const app = fastify();
+app.register(cors, {
+  origin: ["*"],
+});
 
 app.setValidatorCompiler(validatorCompiler);
 app.setSerializerCompiler(serializerCompiler);
@@ -43,6 +46,6 @@ app.register(userController);
 app.register(championshipController);
 app.register(teamController);
 
-app.listen({ port: env.PORT }, () => {
+app.listen({ port: env.PORT, host: "0.0.0.0" }, () => {
   console.log("Server running on port 3333");
 });
