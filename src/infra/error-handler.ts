54cd2ea7bf59655933376a1ef2186dae3d8e8f0c
devictor8 <joaosalgado605@gmail.com>
2025-06-21 09:12:49 -0300

    fix: erros 401 caindo no fallback de erros internos (500)

--- src/infra/error-handler.ts
+++ src/infra/error-handler.ts
@@ -1,4 +1,4 @@
-import { FastifyInstance } from "fastify";
+import { FastifyInstance, FastifyError } from "fastify";
 import { BaseError } from "./exceptions/base-error";
 import { ZodError } from "zod";
 
@@ -16,6 +16,14 @@ export const errorHandler: FastifyErrorHandler = (error, request, reply) => {
   if (error instanceof ZodError) {
     return reply.status(400).send({
       code: 400,
+      error: error.name,
+      message: error.message,
+    });
+  }
+
+  if (error.statusCode === 401) {
+    return reply.status(401).send({
+      code: 401,
       error: error.name,
       message: error.message,
     });
