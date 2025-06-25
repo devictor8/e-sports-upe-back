0a28886fc965e6fd92cae2cf2cbef4c6d0a5c5ec
devictor8 <joaosalgado605@gmail.com>
2025-06-25 14:03:36 -0300

    feat: criando rota para ver todos os usuários (admin)

--- src/controllers/user-controller.ts
+++ src/controllers/user-controller.ts
@@ -3,4 +3,15 @@ import { UserServices } from "../services/user-service";
 import { ZodTypeProvider } from "fastify-type-provider-zod";
 import { z } from "zod/v4";
 
-export async function userController(app: FastifyInstance) {}
+export async function userController(app: FastifyInstance) {
+  app.get(
+    "/users",
+    {
+      onRequest: [app.authAdmin],
+    },
+    async (request, reply) => {
+      const response = await UserServices.getAll();
+      reply.status(200).send(response);
+    }
+  );
+}
